f4841cd0dbeb1a48593f6d063aed3dae3a146d49
Craig <cav9871@saintjoe.edu>
2015-02-11 11:16:38 -0500

    This is my list of movies dataset

---
+++ movie.js
@@ -0,0 +1,73 @@
+var MOIVE_FILE = "[\n " + [
+'{"title": "The Shawshank Redemption", "rating": "9.2", "genre": "crime", "release date": "1994","director" : "Frank Darabont"}',
+ '{"title": "The Godfather", "rating": "9.2", "genre": "crime", "release date": "1972", "director": "Francis Ford Coppola"}',
+ '{"title" : "The Godfather: Part II", "rating": "9.1", "genre": "crime", "release date": "1974", "director": "Francis Ford Coppola"}',
+ '{"title": "The Dark Knight", "rating": "9.0", "genre": "action", "release date": "2008", "director": "Christopher Nolan"}',
+ '{"title": "Pulp Fiction", "rating": "8.9", "genre": "drama", "release date": "1994", "director": "Quentin Tarantino"}',
+
+ '{"title": "12 Angry Men", "rating": "8.9", "genre": "drama", "release date": "1957", "director": "Sidney Lumet"}',
+
+ '{"title": "Schindlers List", "rating": "8.9", "genre": "drama", "release date": "1993", "director": "Steven Spielberg"}',
+ 
+ '{"title": "The Good, the Bad and the Ugly", "rating": "8.9", "genre": "western", "release date": "1996", "director": "Sergio Leone"}',
+
+ '{"title": "The Lord of the Rings, The Return of the King", "rating": "8.9", "genre": "adventure", "release date": "2003", "director": "Peter Jackson"}',
+
+ '{"title": "Fight Club", "rating": "8.9", "genre": "drama", "release date": "1999", "director": "David Fincher"}',
+
+ '{"title": "The Lord of the Rings,The Fellowship of the Ring", "rating": "8.8", "genre": "adventure", "release date": "2001", "director": "Peter Jackson"}',
+
+ '{"title": "Star Wars,Episode V-The Empire Strikes Back", "rating": "8.8", "genre": "fantasy", "release date": "1980", "director": "Irvin Kershner"}',
+
+ '{"title": "Forrest Gump", "rating": "8.8", "genre": "Drama", "release date": "1994", "director": "Robert Zemeckis"}',
+
+ '{"title": "Inception", "rating": "8.8", "genre": "mystery", "release date": "2010", "director": "Christopher Nolan"}',
+
+ '{"title": "Once Flew Over the Cuckoos Nest", "rating": "8.7", "genre": "drama", "release date": "1975", "director": "Milos Forman"}',
+
+ '{"title": "The Lord of the Rings,The Two Towers", "rating": "8.7", "genre": "adventure", "release date": "2002", "director": "Peter Jackson"}',
+
+ '{"title": "Goodfellas", "rating": "8.7", "genre": "crime", "release date": "1990", "director": "Martin Scorsese"}',
+
+ '{"title": "The Matrix", "rating": "8.7", "genre": "action", "release date": "1999", "director": "Andy Wachowski"}',
+
+ '{"title": "Star Wars, Episode IV - A New Hope", "rating": "8.7", "genre": "fantasy", "release date": "1977", "director": "George Lucas"}',
+
+ '{"title": "Seven Samurai", "rating": "8.7", "genre": "drama", "release date": "1954", "director": "Akira Kurosawa"}',
+
+ '{"title": "Interstellar", "rating": "8.7", "genre": "Adventure", "release date": "2014", "director": "Christopher Nolan"}',
+
+ '{"title": "City of God", "rating": "8.7", "genre": "drama", "release date": "2002", "director": "Fernando Meirelles"}',
+
+ '{"title": "Seven", "rating": "8.7", "genre": "thriller", "release date": "1995", "director": "David Fincher"}',
+
+ '{"title": "The Usual Suspects", "rating": "8.7", "genre": "crime", "release date": "1995", "director": "Bryan Singer"}',
+
+ '{"title": "The Silence of the Lambs", "rating": "8.6", "genre": "thriller", "release date": "1991", "director": "Jonathan Demme"}'
+ ]join(",\n ") + "\n]";
+
+ if (typeof module != "undefined" && module.exports) 
+    module.exports = MOIVES_FILE;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
