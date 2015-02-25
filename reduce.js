bb02e95ee89d88a41b95a1f682b68c828a8ef07d
Craig <cav9871@saintjoe.edu>
2015-02-25 11:35:38 -0500

    This is another attempt, it only prints out the first element in the file though

--- reduce.js
+++ reduce.js
@@ -1,13 +1,14 @@
 load('movie.js');
-var movie = JSON.parse(MOIVE_FILE);
+var movie = JSON.parse(MOVIE_FILE);
 
-function reduce(array, combine, start) {
-	  var current = start;
-	    for (var i = 0; i < array.length; i++)
-		        current = combine(current, array[i]);
-	      return current;
+
+var min = movie[0];
+for (var i = 1; i < movie.length; i++) {
+	  var cur = movie[i];
+	    if (cur.release_date < min.realse_date)
+		        min = cur;
 }
-
+print(min);
 
 print(JSON.stringify(movie.reduce(function(min,cur) {
 	    return (cur.release_date < min.release_date)? cur : min;
