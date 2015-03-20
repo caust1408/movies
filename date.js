14effd1215a27e26466433b9b15faae1ec2d1d32
Craig <cav9871@saintjoe.edu>
2015-03-20 12:51:19 -0400

    I fixed the date and it prints out the number of days the movie has been out for

--- date.js
+++ date.js
@@ -8,10 +8,11 @@ for (var i = 1; i < movie.length; i++) {
 	  var cur = movie[i];
 	    Curtime = new Date().getTime();
 	    //print(Curtime);
-	    Oldtime = cur.date;
+	    Oldtime = JSON.parse(cur.date);
+	    //print(Oldtime);
 	    //print('this is time ' + Oldtime);
-	    DaysUntil = (Math.floor(Curtime-Oldtime))/(86400000);
-print(new Date(DaysUntil));
+	    DaysUntil = Math.floor(((Math.abs((Curtime-Oldtime))))/(86400000));
+print(JSON.stringify(cur.title) +' has been out for ' + DaysUntil + ' days');
 }
 //print(min);
 
