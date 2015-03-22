795d41d76864a785eef97c657b1106735ba46944
Craig <cav9871@saintjoe.edu>
2015-03-22 09:58:14 -0400

    I fixed how I had the dates set up

--- movie.js
+++ movie.js
@@ -1,40 +1,40 @@
 var MOVIE_FILE = "[\n " + [
-'{"title": "The Shawshank Redemption", "rating": "9.2", "genre": "crime", "date": "782107200000","director" : "Frank Darabont"}',
- '{"title": "The Godfather", "rating": "9.2", "genre": "crime", "date": "70261200000", "director": "Francis Ford Coppola"}',
- '{"title" : "The Godfather: Part II", "rating": "9.1", "genre": "crime", "date": "156747600000", "director": "Francis Ford Coppola"}',
- '{"title": "The Dark Knight", "rating": "9.0", "genre": "action", "date": "1216353600000", "director": "Christopher Nolan"}',
- '{"title": "Pulp Fiction", "rating": "8.9", "genre": "drama", "date": "782107200000", "director": "Quentin Tarantino"}',
- '{"title": "12 Angry Men", "rating": "8.9", "genre": "drama", "date": "760338000000", "director": "Sidney Lumet"}',
- '{"title": "Schindlers List", "rating": "8.9", "genre": "drama", "date": "-401140800000", "director": "Steven Spielberg"}',
- '{"title": "The Good, the Bad and the Ugly", "rating": "8.9", "genre": "western", "date": "-95454000000", "director": "Sergio Leone"}',
- '{"title": "The Lord of the Rings, The Return of the King", "rating": "8.9", "genre": "adventure", "date": "1071637200000", "director": "Peter Jackson"}',
- '{"title": "Fight Club", "rating": "8.9", "genre": "drama", "date": "939960000000", "director": "David Fincher"}',
- '{"title": "The Lord of the Rings,The Fellowship of the Ring", "rating": "8.8", "genre": "adventure", "date": "1008738000000", "director": "Peter Jackson"}',
- '{"title": "Star Wars,Episode V-The Empire Strikes Back", "rating": "8.8", "genre": "fantasy", "date": "330321600000", "director": "Irvin Kershner"}',
- '{"title": "Forrest Gump", "rating": "8.8", "genre": "Drama", "date": "773467200000", "director": "Robert Zemeckis"}',
- '{"title": "Inception", "rating": "8.8", "genre": "mystery", "date": "1279252800000", "director": "Christopher Nolan"}',
+'{"title": "The Shawshank Redemption", "rating": "9.2", "genre": "crime", "date": "14-10-1994","director" : "Frank Darabont"}',
+ '{"title": "The Godfather", "rating": "9.2", "genre": "crime", "date": "24-3-1972", "director": "Francis Ford Coppola"}',
+ '{"title" : "The Godfather: Part II", "rating": "9.1", "genre": "crime", "date": "20-12-1974", "director": "Francis Ford Coppola"}',
+ '{"title": "The Dark Knight", "rating": "9.0", "genre": "action", "date": "18-7-2008", "director": "Christopher Nolan"}',
+ '{"title": "Pulp Fiction", "rating": "8.9", "genre": "drama", "date": "14-10-1994", "director": "Quentin Tarantino"}',
+ '{"title": "12 Angry Men", "rating": "8.9", "genre": "drama", "date": "16-4-1957", "director": "Sidney Lumet"}',
+ '{"title": "Schindlers List", "rating": "8.9", "genre": "drama", "date": "4-2-1994", "director": "Steven Spielberg"}',
+ '{"title": "The Good, the Bad and the Ugly", "rating": "8.9", "genre": "western", "date": "23-12-1966", "director": "Sergio Leone"}',
+ '{"title": "The Lord of the Rings, The Return of the King", "rating": "8.9", "genre": "adventure", "date": "17-12-2003", "director": "Peter Jackson"}',
+ '{"title": "Fight Club", "rating": "8.9", "genre": "drama", "date": "15-10-1999", "director": "David Fincher"}',
+ '{"title": "The Lord of the Rings,The Fellowship of the Ring", "rating": "8.8", "genre": "adventure", "date": "19-12-2001", "director": "Peter Jackson"}',
+ '{"title": "Star Wars,Episode V-The Empire Strikes Back", "rating": "8.8", "genre": "fantasy", "date": "20-6-1980", "director": "Irvin Kershner"}',
+ '{"title": "Forrest Gump", "rating": "8.8", "genre": "Drama", "date": "6-7-1994", "director": "Robert Zemeckis"}',
+ '{"title": "Inception", "rating": "8.8", "genre": "mystery", "date": "16-7-2010", "director": "Christopher Nolan"}',
 
- '{"title": "Once Flew Over the Cuckoos Nest", "rating": "8.7", "genre": "drama", "date": "18577800000", "director": "Milos Forman"}',
+ '{"title": "Once Flew Over the Cuckoos Nest", "rating": "8.7", "genre": "drama", "date": "21-11-1975", "director": "Milos Forman"}',
 
- '{"title": "The Lord of the Rings,The Two Towers", "rating": "8.7", "genre": "adventure", "date": "1040187600000", "director": "Peter Jackson"}',
+ '{"title": "The Lord of the Rings,The Two Towers", "rating": "8.7", "genre": "adventure", "date": "18-12-2002", "director": "Peter Jackson"}',
 
- '{"title": "Goodfellas", "rating": "8.7", "genre": "crime", "date": "653889600000", "director": "Martin Scorsese"}',
+ '{"title": "Goodfellas", "rating": "8.7", "genre": "crime", "date": "21-10-1990", "director": "Martin Scorsese"}',
 
- '{"title": "The Matrix", "rating": "8.7", "genre": "action", "date": "922856400000", "director": "Andy Wachowski"}',
+ '{"title": "The Matrix", "rating": "8.7", "genre": "action", "date": "31-3-1999", "director": "Andy Wachowski"}',
 
- '{"title": "Star Wars, Episode IV - A New Hope", "rating": "8.7", "genre": "fantasy", "date": "236059200000", "director": "George Lucas"}',
+ '{"title": "Star Wars, Episode IV - A New Hope", "rating": "8.7", "genre": "fantasy", "date": "25-5-1977", "director": "George Lucas"}',
 
- '{"title": "Seven Samurai", "rating": "8.7", "genre": "drama", "date": "-477082800000", "director": "Akira Kurosawa"}',
+ '{"title": "Seven Samurai", "rating": "8.7", "genre": "drama", "date": "19-11-1954", "director": "Akira Kurosawa"}',
 
- '{"title": "Interstellar", "rating": "8.7", "genre": "Adventure", "date": "1030680000000", "director": "Christopher Nolan"}',
+ '{"title": "Interstellar", "rating": "8.7", "genre": "Adventure", "date": "7-11-2014", "director": "Christopher Nolan"}',
 
- '{"title": "City of God", "rating": "8.7", "genre": "drama", "date": "141533640000", "director": "Fernando Meirelles"}',
+ '{"title": "City of God", "rating": "8.7", "genre": "drama", "date": "30-8-2002", "director": "Fernando Meirelles"}',
 
- '{"title": "Seven", "rating": "8.7", "genre": "thriller", "date": "811742400000", "director": "David Fincher"}',
+ '{"title": "Seven", "rating": "8.7", "genre": "thriller", "date": "22-9-1995", "director": "David Fincher"}',
 
- '{"title": "The Usual Suspects", "rating": "8.7", "genre": "crime", "date": "811137600000", "director": "Bryan Singer"}',
+ '{"title": "The Usual Suspects", "rating": "8.7", "genre": "crime", "date": "15-9-1995", "director": "Bryan Singer"}',
 
- '{"title": "The Silence of the Lambs", "rating": "8.6", "genre": "thriller", "date": "666507600000", "director": "Jonathan Demme"}'
+ '{"title": "The Silence of the Lambs", "rating": "8.6", "genre": "thriller", "date": "14-2-1991", "director": "Jonathan Demme"}'
  ].join(",\n ") + "\n]";
 
  if (typeof module != "undefined" && module.exports) 
