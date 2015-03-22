4ff559b881579265983c43b6407b42869683af83
Craig <cav9871@saintjoe.edu>
2015-03-22 10:00:24 -0400

    Added new function to give me the date in milliseconds

--- date.js
+++ date.js
@@ -3,15 +3,23 @@ load('movie.js');
 var movie = JSON.parse(MOVIE_FILE);
 
 
-var min = movie[0];
+function getDate(string) {
+	var date = /(\d{1,2})-(\d{1,2})-(\d{4})/;
+	var match = date.exec(string);
+	var output = new Date(Number(match[3]),Number(match[2]) - 1,Number(match[1]));
+	return output.getTime();
+	}
+
 for (var i = 1; i < movie.length; i++) {
 	  var cur = movie[i];
+	  //var date = JSON.parse(cur.date);
+
 	    Curtime = new Date().getTime();
 	    //print(Curtime);
-	    Oldtime = JSON.parse(cur.date);
-	    //print(Oldtime);
+	    Oldtime = getDate(cur.date);
+	    //print(Oldti;
 	    //print('this is time ' + Oldtime);
-	    DaysUntil = Math.floor(((Math.abs((Curtime-Oldtime))))/(86400000));
+	    DaysUntil = Math.ceil(((Math.abs((Curtime-Oldtime))))/(86400000));
 print(JSON.stringify(cur.title) +' has been out for ' + DaysUntil + ' days');
 }
 //print(min);
